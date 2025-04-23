d6f8a89a7a0d764a95ff9fa67e76ac50643ccb58
k7aled <khd.rashidi@gmail.com>
2025-04-24 03:16:25 +0330

    import CtaSection in App.vue

--- src/App.vue
+++ src/App.vue
@@ -2,15 +2,28 @@
   <q-layout view="lHh Lpr lFf">
     <!-- Navbar -->
     <the-header />
+    <q-page-container>
+      <q-page class="bg-dark q-pa-xl">
+        <!-- CTA Section -->
+        <cta-section @back="handleBack" />
+      </q-page>
+    </q-page-container>
   </q-layout>
 </template>
 <script>
 import TheHeader from "./components/TheHeader.vue";
+import CtaSection from "./components/CtaSection.vue";
 
 export default {
   name: "App",
   components: {
     TheHeader,
+    CtaSection,
+  },
+  methods: {
+    handleBack() {
+      console.log("Back button clicked");
+    },
   },
 };
 </script>
