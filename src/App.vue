cd65858ea7a1c1ffef1971f6017caadc1d1c6fe0
k7aled <khd.rashidi@gmail.com>
2025-04-24 02:36:49 +0330

    init project

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <div>Movie web design</div>
+  </q-layout>
+</template>
+
+<script>
+export default {
+  name: "App",
+};
+</script>
+
+<style lang="scss"></style>
