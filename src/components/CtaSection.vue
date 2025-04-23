ec151b9e31734cb59e0160e51da4b7af46b4b884
k7aled <khd.rashidi@gmail.com>
2025-04-24 03:15:56 +0330

    create "CtaSection" component

---
+++ src/components/CtaSection.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="row items-center q-mb-md">
+    <q-btn
+      flat
+      dense
+      icon="arrow_back"
+      :label="label"
+      class="bg-grey-10 text-white"
+      @click="$emit('back')" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "CtaSection",
+  props: {
+    label: {
+      type: String,
+      default: "Back",
+    },
+  },
+};
+</script>
+
+<style scoped></style>
