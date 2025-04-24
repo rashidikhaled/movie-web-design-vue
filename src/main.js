690f2bf23e2d49f1c10c3b34f19d3aca127b4668
k7aled <khd.rashidi@gmail.com>
2025-04-24 11:38:35 +0330

    add font style and imdb logo

--- src/main.js
+++ src/main.js
@@ -8,6 +8,7 @@ import "@quasar/extras/material-icons/material-icons.css";
 
 // Import Quasar css
 import "quasar/src/css/index.sass";
+import "./style.css";
 
 // Assumes your root component is App.vue
 // and placed in same folder as main.js
